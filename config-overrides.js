f8c2d319cb373e616027a071829bd5ef22799500
Coleman Kelleghan <imdesigner2012@gmail.com>
2022-04-09 12:50:11 -0600

    Config override

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+
+module.exports = function override(config, env) {
+  if (!config.plugins) {
+    config.plugins = [];
+  }
+
+  if (!config?.module?.rules) {
+    config.module.rules = [];
+  }
+
+  config.module.rules.push(  {
+    test: /\.(jpe?g|png|webp|avif|tiff)$/i,
+    type: 'asset',
+    generator: {
+      filename: () =>  '[name].[contenthash:8].webp'
+    }
+  })
+
+
+  config.plugins.push(
+    new ImageminWebpWebpackPlugin()
+  )
+
+  return config;
+}
